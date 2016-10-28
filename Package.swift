f7f1c1fe72792fb0b354e92e42075a2705b14db9
first commit <rajeshn597@gmail.com>
2016-10-28 15:29:28 +0530

    removed

--- Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-  name: "HelloWorld",
-  dependencies: [
-    .Package(url: "https://github.com/kylef/Curassow.git", majorVersion: 0, minor: 2),
-  ]
-)
